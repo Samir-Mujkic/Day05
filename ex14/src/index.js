5f88c76a95403af7cb392429f9c72c18629425ea
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-12 15:18:55 +0100

    zadatak 14

---
+++ ex14/src/index.js
@@ -0,0 +1,22 @@
+function main(){
+
+    // Only change code below this line
+      var myNoun = "dog";
+      var myVerb = "ran";
+      var myAdjective = "big";
+      var myAdverb = "quickly";
+      var wordBlanks = "With " +  myNoun +  " i " +  myVerb +  " very " +  myAdjective + " and " + myAdverb + "."  ;
+      
+      
+
+
+
+    // Onyl change code above this line
+
+    return wordBlanks;
+        
+       
+    
+}
+      console.log(main());
+      module.exports = main;
