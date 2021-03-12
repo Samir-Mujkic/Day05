bd79b59e9d274fa211518f9b363d4ea1ae6b4f9d
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-12 10:16:29 +0100

    zadatak 1

---
+++ ex01/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+
+    // Only change code below this line
+      var myFirstString = "I think therefore i \"am\" -- sentient.";
+      var mySecondString ="No, you only \"think\", so you aren't.";
+
+
+
+    // Onyl change code above this line
+
+    return{
+        myFirstString,
+        mySecondString
+    };
+}
+
+console.log(main());
+module.exports = main;
