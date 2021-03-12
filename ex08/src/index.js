80c5cd23aeb4e6115e82c8cb3ffd467c2fc141d7
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-12 13:27:14 +0100

    zadatak 8

---
+++ ex08/src/index.js
@@ -0,0 +1,21 @@
+function main(){
+
+    // Only change code below this line
+      var lastName = "Doe";
+      var lastNameLength = lastName.length ;
+      
+      
+      
+
+
+
+    // Onyl change code above this line
+
+    return lastNameLength;
+        
+       
+    
+}
+
+console.log(main());
+module.exports = main
