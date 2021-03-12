ce6ca1648e6d83813fac74936b98abefb97fdd14
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-12 10:37:27 +0100

    zadatak 2

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+
+    // Only change code below this line
+      var myLink = ' a href = "http ://www.example.com " target = "_blank ">Link<a '
+      
+
+
+
+    // Onyl change code above this line
+
+    return myLink;
+        
+       
+    
+}
+
+console.log(main());
+module.exports = main;
