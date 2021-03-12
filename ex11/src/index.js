7e97dff319d3bf0254de4498656e1a1a88d97fa0
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-12 13:55:42 +0100

    zadatak 11

---
+++ ex11/src/index.js
@@ -0,0 +1,20 @@
+function main(){
+
+    // Only change code below this line
+      var lastName = "Tesla";
+      var thirdLetterOfLastName = lastName[2];
+      
+      
+
+
+
+    // Onyl change code above this line
+
+    return thirdLetterOfLastName;
+        
+       
+    
+}
+
+console.log(main());
+module.exports = main
