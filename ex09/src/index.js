a7899027ee9caee850603853ee883b55d92795dc
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-12 13:42:16 +0100

    zadatak 9

---
+++ ex09/src/index.js
@@ -0,0 +1,20 @@
+function main(){
+
+    // Only change code below this line
+      var LastName = "Doe";
+      var firstLetterOfLastName = LastName[0];
+      
+      
+
+
+
+    // Onyl change code above this line
+
+    return firstLetterOfLastName;
+        
+       
+    
+}
+
+console.log(main());
+module.exports = main
