67bc0543d71a1462ef426744c905e5c78f6aefab
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-12 11:08:29 +0100

    zadatak 3

---
+++ ex03/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+
+    // Only change code below this line
+      var myString = "FirstLine\n\tSecondLine\nThirdLine"
+      
+
+
+
+    // Onyl change code above this line
+
+    return myString;
+        
+       
+    
+}
+
+console.log(main());
+module.exports = main;
