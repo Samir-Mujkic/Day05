4193f53622cc770b04743237ef73e873a84831a7
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-12 18:08:04 +0100

    zadatak1

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,7 +1,7 @@
 function main(){
 
     // Only change code below this line
-      var myFirstString = "I think therefore i \"am\" -- sentient.";
+      var myFirstString = "I think therefore I \"am\" -- sentient.";
       var mySecondString = "No, you only \"think\", so you aren't.";
 
 
